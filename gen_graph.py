1c64509b6104ddfc48939d1e1978922f9a71fe5b
Alexander Neupokoev <admin@armavir.ru>
2020-03-18 14:27:57 +0300

    Add crontab gen_graph.py

---
+++ gen_graph.py
@@ -0,0 +1,45 @@
+import plotly.express as px
+import pandas as pd
+import datetime
+
+import parse
+
+def get_date_and_time():
+	today = datetime.datetime.today()
+	date_and_time_today = today.strftime("%d.%m.%Y %H:%M")
+	return date_and_time_today
+
+def create_graph(table):
+	df = pd.DataFrame(table, columns=['Страны', 'Cases', 'Deaths'])
+
+	fig = px.bar(df, x = "Cases", y = "Страны", 
+		title=f"Зараженные COVID-19. TOP20 + RU {get_date_and_time()}", 
+		orientation='h',
+		hover_data=['Deaths'], text='Cases',
+		height=800, width=600
+		)
+	fig.update_traces(textposition='auto')
+	fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+	fig.write_image("cases_top20.png")
+
+def create_graph_deaths(table):
+	df = pd.DataFrame(table, columns=['Страны', 'Cases', 'Deaths'])
+	 
+	fig = px.bar(df, x = "Deaths", y = "Страны",
+		title=f"Умершие от COVID-19. {get_date_and_time()}",
+		orientation='h', text='Deaths',
+		height=800, width=600
+		)
+	fig.update_traces(textposition='auto')
+	fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+	fig.write_image("deaths.png")
+
+
+def main():
+	url = 'https://www.worldometers.info/coronavirus/'
+	table = parse.get_from_countries_covid(parse.get_html(url), 20)
+	create_graph(table)
+	create_graph_deaths(table)
+
+if __name__ == '__main__':
+	main()
--- main.py
+++ main.py
@@ -9,12 +9,6 @@ import tg, parse, graph
 
 def main():
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-    url = 'https://www.worldometers.info/coronavirus/'
-    # total = get_total_covid(get_html(url))
-    # print(f'Всего заболевших: {total}')
-    table = parse.get_from_countries_covid(parse.get_html(url), 20)
-    graph.create_graph(table)
-    graph.create_graph_deaths(table)
     tg.launch_bot(TELEGRAM_TOKEN)
 
 if __name__ == '__main__':
