2e2c5fb213c7c2d25962bb0d9cf079358ee51da7
Alexander Neupokoev <admin@armavir.ru>
2020-03-25 15:45:13 +0300

    Add date and time ti graphics

--- total_graphs.py
+++ total_graphs.py
@@ -2,28 +2,30 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+from graph import get_date_and_time
+
 
 def main():
 	total = pd.read_csv('db/total.csv', delimiter=';', 
 		names=['Дата', 'Случаи', 'Умершие', 'Выздоровевшие'])	
 	total_cases = total[['Дата', 'Случаи']]
 	cases_fig = px.line(total_cases, x = 'Дата', y = 'Случаи', height = 800, width = 600, 
-		title = 'График по количеству зараженных COVID-19')
+		title = f'График по количеству зараженных COVID-19. {get_date_and_time()}')
 
 	total_deaths = total[['Дата', 'Умершие']]
 	deaths_fig = px.line(total_deaths, x = 'Дата', y = 'Умершие', height = 800, width = 600, 
-		title = 'График по умершим от COVID-19')
+		title = f'График по умершим от COVID-19. {get_date_and_time()}')
 
 	total_recovered = total[['Дата', 'Выздоровевшие']]
 	recovered_fig = px.line(total_recovered, x = 'Дата', y = 'Выздоровевшие', height = 800, width = 600,
-		title = 'График по выздоровевшим от COVID-19')
+		title = f'График по выздоровевшим от COVID-19. {get_date_and_time()}')
 
 	countries = pd.read_csv('db/countries.csv', delimiter=';', 
 		names=['Дата', 'Страна', 'Случаи', 'Умершие', 'Выздоровевшие'])
 	russia = countries[countries.Страна == 'Russia']
 	rus_case = russia[['Дата', 'Случаи']]
 	rus_fig = px.line(rus_case, x = 'Дата', y = 'Случаи', height = 800, width = 600,
-                 title = 'График заражения в России')
+                 title = f'График заражения в России. {get_date_and_time()}')
 
 	cases_fig.write_image("total_cases.png")
 	deaths_fig.write_image('total_deaths.png')
